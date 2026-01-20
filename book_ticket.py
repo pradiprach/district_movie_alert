95c2fb2fc4b23665285288526bf9f0b0e0abf141
Pradip Rachamalla <pradeeprach@gmail.com>
2026-01-20 12:26:39 +0530

    Update book_ticket.py

--- book_ticket.py
+++ book_ticket.py
@@ -3,6 +3,8 @@ import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from smtplib import SMTP
+from datetime import datetime, date
+from zoneinfo import ZoneInfo
 
 import requests
 from bs4 import BeautifulSoup
@@ -27,26 +29,37 @@ def send_email():
         connection.login(user=my_email, password=password)
         connection.sendmail(from_addr=my_email, to_addrs=to_email, msg=message.as_string())
 
-url = "https://www.district.in/movies/anaganaga-oka-raju-movie-tickets-in-hyderabad-MV151347?frmtid=sXBaVe4G5&fromdate=2026-01-17"
+url = f"https://www.district.in/movies/anaganaga-oka-raju-movie-tickets-in-hyderabad-MV151347?frmtid=sXBaVe4G5&fromdate={date}"
 content = requests.get(url=url).text
 soup = BeautifulSoup(content, 'html.parser')
 script = soup.find("script", id="__NEXT_DATA__")
 json_data = json.loads(script.text)
 data = json_data["props"]["pageProps"]["data"]["serverState"]["movieSessions"]
 
-is_found = False
-for session in data:
-    shows = data[session]["arrangedSessions"]
-    for show in shows:
-        show_data = show["data"]
-        if cinema_name in show_data["name"]:
-            send_email()
-            is_found = True
-            break
-if is_found:
-    print("Tickets Available!")
+given_dt_ist = datetime.strptime(
+    date, "%Y-%m-%d"
+).replace(
+    tzinfo=ZoneInfo("Asia/Kolkata")
+)
+today_ist = datetime.now(ZoneInfo("Asia/Kolkata"))
+
+if today_ist >= given_dt_ist:
+    print("Date exceeded or today is current date. Hence Skipping!")
 else:
-    print("Tickets not available")
+    is_found = False
+    for session in data:
+        shows = data[session]["arrangedSessions"]
+        for show in shows:
+            show_data = show["data"]
+            if cinema_name in show_data["name"]:
+                send_email()
+                is_found = True
+                break
+    if is_found:
+        print("Tickets Available!")
+    else:
+        print("Tickets not available")
+
 
 
 
